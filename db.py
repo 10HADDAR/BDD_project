3cc74fe1b823375c045c258f26de85ccbb12ef72
10HADDAR <haddar10massa@gmail.com>
2026-01-20 11:44:50 +0100

    Ready for Render

--- db.py
+++ db.py
@@ -1,5 +1,18 @@
 import os
 import psycopg2
+from urllib.parse import urlparse
 
 def get_conn():
-    return psycopg2.connect(os.environ["postgresql://exams_db_user:9Rob3mujL5dIt5G2ze7GOG8lCAoqCVYd@dpg-d5nd24hr0fns73fgrgbg-a/exams_db"])
+    DATABASE_URL = os.environ.get("DATABASE_URL")
+    if not DATABASE_URL:
+        # Valeur par défaut locale pour tests
+        DATABASE_URL = "postgresql://postgres:postgres@localhost:5432/exams_db"
+
+    result = urlparse(DATABASE_URL)
+    return psycopg2.connect(
+        dbname=result.path[1:],
+        user=result.username,
+        password=result.password,
+        host=result.hostname,
+        port=result.port
+    )
